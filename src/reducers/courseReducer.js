b7054ee77830c7357d9f79c2198f0e42ead10f3f
Jerry Relunia <jrelunia@team.com>
2016-09-11 17:18:13 -0400

    adding a rootReducer that combines the reducers together

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -4,6 +4,7 @@ export default function courseReducer(state = [], action) {
             //state.push(action.course); (don't do this, state should be immutable)
             //return state;
             
+            //this way copies over a new array with an extra value inside
             return [...state,
                 Object.assign({}, action.course)
             ];
