45eda2113742d9eebe758aa3668224bf72a42192
Jerry Relunia <jrelunia@team.com>
2016-09-11 13:06:29 -0400

    revert back

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -2,27 +2,6 @@ import React, { PropTypes } from 'react';
 
 class CoursesPage extends React.Component {
     
-    constructor(props, component) {
-        super(props,context);
-        
-        this.state = {
-            course: { title: "" }
-        };
-        this.comment = "Place your bind calls on the constructor, not the render";
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-    
-    onTitleChange(e) {
-        const course = this.state.course;
-        course.title = e.target.value;
-        this.setState({course: course});
-    }
-    
-    onClickSave() {
-        alert(`Saving ${this.state.course.title}`);
-    }
-    
     render() {
         return (
             <div>
