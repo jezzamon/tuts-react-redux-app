f931fd7c26d991e628e0db534a2acfe18814b391
Jerry Relunia <jrelunia@team.com>
2016-09-11 12:46:43 -0400

    add onChange function to coursepage component

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,10 +1,35 @@
 import React, { PropTypes } from 'react';
 
 class CoursesPage extends React.Component {
+    
+    constructor(props, component) {
+        super(props,context);
+        
+        this.state = {
+            course: { title: null }
+        };
+    }
+    
+    onTitleChange(e) {
+        const course = this.state.course;
+        course.title = e.target.value;
+        this.setState({course: course});
+    }
+    
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                <h2>Add courses</h2>
+                <input 
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title} />
+            
+                <input
+                    type="submit"
+                    value="Save"
+                    onClick={this.onClickSave} />
             </div>
         );
     }
