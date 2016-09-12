a10f5fe5d12d3a54fe6a2e3cfc4df3da38b00e26
Jerry Relunia <jrelunia@team.com>
2016-09-12 13:47:03 -0400

    bind action creators using redux helper

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
@@ -22,7 +23,7 @@ class CoursesPage extends React.Component {
     }
     
     onClickSave() {
-        this.props.createCourse(this.state.course);
+        this.props.actions.createCourse(this.state.course);
     }
     
     courseRow(course, index) {
@@ -52,7 +53,7 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
 //    dispatch: PropTypes.func.isRequired,  //no longer needed with mapDispatchToProps in connect
     courses: PropTypes.array.isRequired,
-    createCourse: PropTypes.func.isRequired
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state,ownProps) {
@@ -62,9 +63,10 @@ function mapStateToProps(state,ownProps) {
 }
 
 //determines what actions are in our component
+    //bindActionCreators will go through all the actions in courseActions file and wrap them in a dispatch
 function mapDispatchToProps(dispatch) {
     return {
-        createCourse: (course) => dispatch(courseActions.createCourse(course))
+        actions: bindActionCreators(courseActions, dispatch)
     };
 }
 
