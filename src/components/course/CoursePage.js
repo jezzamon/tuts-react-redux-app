169a54797de151ba3c551b5e1b75584c77559186
Jerry Relunia <jrelunia@team.com>
2016-09-13 17:00:46 -0400

    dispatch create and update

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -3,14 +3,19 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import { browserHistory } from 'react-router';
 
 class CoursesPage extends React.Component {
  
     constructor(props, context) { //1
         super(props, context);
+		this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
     }
     /***********************************/
     
+	redirectToAddCoursePage() {
+		browserHistory.push('/course');
+	}
     //Render components - ideally they just call components rather than inline here
     render() {
         const {courses} = this.props;
@@ -18,6 +23,10 @@ class CoursesPage extends React.Component {
         return (
             <div>
                 <h1>Courses</h1>
+				<input type="submit"
+					value="Add Course"
+					className="btn btn-primary"
+					onClick={this.redirectToAddCoursePage}/>
                 <CourseList courses={courses} />
             </div>
         );
