6b151abb26ec0c2569e56977706c58588bb3f9ca
Jerry Relunia <jrelunia@team.com>
2016-09-12 21:03:25 -0400

    debug

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    
+    render() {
+        return (
+        <p>Manage Course</p>
+        );
+    }
+}
+
+ManageCoursePage.propTypes = {
+    //myProp: PropTypes.string.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+      state: state  
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage
+);
