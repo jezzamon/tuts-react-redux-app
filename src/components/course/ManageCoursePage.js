ca4846b71293f2027bdce9e2453a7a25723482d4
Jerry Relunia <jrelunia@team.com>
2016-09-13 17:36:12 -0400

    populate form via mapStateToProps

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -54,9 +54,25 @@ ManageCoursePage.contextTypes = {
 	router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+	debugger;
+	const course = courses.filter(course => courses.id == id);
+	if (course.length) {
+		return course[0]; //since filter return an array, grab first
+	} else {
+		return null
+	}
+}
+
 function mapStateToProps(state, ownProps) {
+	const courseId = ownProps.params.id; // from the path '/course/:id'
+	
 	let course = {id:'', watchHref:'', title:'', authorId:'', length:'', category:''};
     
+	if (courseId && state.courses.length > 0) {
+		course = getCourseById(state.courses, courseId);
+	}
+	
 	const authorsFormattedForDropdown = state.authors.map(author => {
 		return {
 			value: author.id,
