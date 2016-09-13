7b79f277b25afe0f1d8db576b15276ef4122a275
Jerry Relunia <jrelunia@team.com>
2016-09-13 16:23:46 -0400

    create save course thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,14 @@ export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses: courses};
 }
 
+export function createCourseSuccess(course) {
+    return { type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function udpateCourseSuccess(course) {
+    return { type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
 //thunk actions (thunk always returns a function that accepts a dispatch)
 export function loadCourses() {
     return function(dispatch) {
@@ -15,4 +23,16 @@ export function loadCourses() {
            throw(error); 
         });
     };
+}
+
+export function saveCourse(course) {
+	return function (dispatch, getState) {  //getState optional
+		return courseApi.saveCourse(course).then(savedCourse => {
+			course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+				dispatch(createCourseSuccess(savedCourse));
+		})
+		.catch( error => {
+			throw(error);
+		});
+	};
 }
