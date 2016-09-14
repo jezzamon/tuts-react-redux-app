b9fc3843b63e3fbc2a47de02fcb0d595fbbcc3ef
Jerry Relunia <jrelunia@team.com>
2016-09-14 15:03:18 -0400

    create ajax status reducer

---
+++ src/reducers/ajaxStatusReducer.js
@@ -0,0 +1,20 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+//any action type that ends in SUCCESS will not be handled here, meaning the same action can be handled in multiple reducers.
+//remember each reducer just handles a a slice of state
+
+
+function actionTypeEndsInSuccess (type) {
+	return type.substring(type.length - 8) == '_SUCCESS';
+}
+
+export default function ajaxStatusReducer (state = initialState.ajaxCallsInProgress, action) {
+	if (action.type == types.BEGIN_AJAX_CALL) {
+		return state + 1;
+	} else if (actionTypeEndsInSuccess(action.type)) {
+		return state - 1;
+	}
+
+	return state;
+}
