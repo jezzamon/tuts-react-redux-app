272281b989b77ba9ba9b41131ba1b43df918cc7d
Jerry Relunia <jrelunia@team.com>
2016-09-14 15:08:07 -0400

    call begin AJAX in thunks

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import authorApi from '../api/mockAuthorApi';
+import { beginAjaxCall } from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
     return { type: types.LOAD_AUTHORS_SUCCESS, authors: authors};
@@ -8,6 +9,7 @@ export function loadAuthorsSuccess(authors) {
 //thunk actions (thunk always returns a function that accepts a dispatch)
 export function loadAuthors() {
     return (dispatch) => {
+		dispatch(beginAjaxCall());
         return authorApi.getAllAuthors().then(authors => {
             dispatch(loadAuthorsSuccess(authors));
         })
