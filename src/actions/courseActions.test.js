d7fc1f7f2c93813e67d0b3ef25667b82a4411ea9
Jerry Relunia <jrelunia@team.com>
2016-09-15 13:40:30 -0400

    testing thunks - async calls

--- src/actions/courseActions.test.js
+++ src/actions/courseActions.test.js
@@ -2,6 +2,10 @@ import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
 
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+
 //Test a sync action
 describe('Course Actions', () => {
 	describe('CreateCourseSuccess', () => {
@@ -20,4 +24,25 @@ describe('Course Actions', () => {
 			expect(action).toEqual(expectedAction);
 		});
 	});
+});
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async actions', () => {
+
+	afterEach(() => {
+		nock.cleanAll();
+	});
+
+	//note for async we pass (done) as an argument
+	it('should create BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when loading course', (done) => {
+		// Here's an example call to nock
+
+//		nock('http://example.com')
+//			.get('/courses')
+//			.reply(200, {body: { course: [{ id:1, firstName: 'Cory', lastName: 'House'}] }});
+
+	});
+
 });
